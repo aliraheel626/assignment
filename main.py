d50ac36d5d7806b716fa51a6e7be074519ca45f0
Ali <aliraheel626@gmail.com>
2025-11-29 09:15:28 +0500

    removed reload

--- main.py
+++ main.py
@@ -28,4 +28,4 @@ def delete_item(item_id: int):
     return {"message": "Item deleted"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
